bba5441b99fd12fa6c2bd2674dfcd81c25dd37a1
David <davidyen1124@gmail.com>
2014-06-01 18:24:55 +0800

    improve code

--- shooter.js
+++ shooter.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var shtg_calcfilehash = require('./shtg_calcfilehash');
 
 var shooter_url = 'http://www.shooter.cn';
 var shtg_filehash = 'duei7chy7gj59fjew73hdwh213f';
 
 // check user gives a valid query string
 if (process.length < 3) {
-    console.log('node shooter.js <query>');
+    console.log('node shooter.js <query> [output path]');
     process.exit(1);
 }
 
@@ -103,107 +104,37 @@ function downloadSub(url, callback) {
             }
         });
 
+        // we only want zh-TW subtitles
+        if (subtitle.indexOf('繁') < 0) {
+            return callback();
+        }
+
         // use the hashurl to get the hash text,
         // then use shtg_calcfilehash() to build a complete url
         request(hashUrl, function(err, res, body) {
             var hash = shtg_calcfilehash(body);
             var url = 'http://file1.shooter.cn' + hash;
 
-            // we only want need zh-TW subtitle
-            if (subtitle.indexOf('繁') >= 0) {
-                var path = '';
-                if (process.argv[3] !== undefined) {
-                    path = process.argv[3];
-                }
+            var directory = '';
+            var filename = '';
 
-                var match = /file1\.shooter\.cn\/c\/(.*?)\?/i.exec(url);
-                var file;
-                if (match && match.length > 0) {
-                    path += match[1];
-                    file = fs.createWriteStream(path);
-                } else {
-                    path += id + '.rar';
-                    file = fs.createWriteStream(path);
-                }
-
-                console.log('download', id, url);
-                request(url, function(err) {
-                    if (err) {
-                        return;
-                    }
-                }).pipe(file);
+            // if user gave the output directory parameter,
+            // then set directory to it
+            if (process.argv[3] !== undefined) {
+                directory = process.argv[3];
+                path = directory;
             }
+
+            var match = /file1\.shooter\.cn\/c\/(.*?)\?/i.exec(url);
+            if (match && match.length > 0) {
+                filename = match[1];
+            } else {
+                filename = id + '.rar';
+            }
+            var file = fs.createWriteStream(directory + filename);
+
+            console.log('download', id, url);
+            request(url).pipe(file);
         });
     });
-}
-
-function shtg_calcfilehash(a) {
-    function b(j) {
-        var g = "";
-        for (var f = 0; f < j.length; f++) {
-            var h = j.charCodeAt(f);
-            g += (h + 47 >= 126) ? String.fromCharCode(" ".charCodeAt(0) + (h +
-                47) % 126) : String.fromCharCode(h + 47)
-        }
-        return g
-    }
-
-    function d(g) {
-        var j = g.length;
-        j = j - 1;
-        var h = "";
-        for (var f = j; f >= 0; f--) {
-            h += (g.charAt(f))
-        }
-        return h
-    }
-
-    function c(j, h, g, f) {
-        return j.substr(j.length - f + g - h, h) + j.substr(j.length - f, g - h) +
-            j.substr(j.length - f + g, f - g) + j.substr(0, j.length - f)
-    }
-    if (a.length > 32) {
-        switch (a.charAt(0)) {
-            case "o":
-                return (b((c(a.substr(1), 8, 17, 27))));
-                break;
-            case "n":
-                return (b(d(c(a.substr(1), 6, 15, 17))));
-                break;
-            case "m":
-                return (d(c(a.substr(1), 6, 11, 17)));
-                break;
-            case "l":
-                return (d(b(c(a.substr(1), 6, 12, 17))));
-                break;
-            case "k":
-                return (c(a.substr(1), 14, 17, 24));
-                break;
-            case "j":
-                return (c(b(d(a.substr(1))), 11, 17, 27));
-                break;
-            case "i":
-                return (c(d(b(a.substr(1))), 5, 7, 24));
-                break;
-            case "h":
-                return (c(b(a.substr(1)), 12, 22, 30));
-                break;
-            case "g":
-                return (c(d(a.substr(1)), 11, 15, 21));
-            case "f":
-                return (c(a.substr(1), 14, 17, 24));
-            case "e":
-                return (c(a.substr(1), 4, 7, 22));
-            case "d":
-                return (d(b(a.substr(1))));
-            case "c":
-                return (b(d(a.substr(1))));
-            case "b":
-                return (d(a.substr(1)));
-            case "a":
-                return b(a.substr(1));
-                break
-        }
-    }
-    return a
 }
